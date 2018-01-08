8f008e8e4fd5e31a54fb9cbf9bdaae106670b519
fellipe-pc <fellipe-pc@fellipe-pc>
2018-01-07 23:18:51 -0200

    primeiro commit real

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php 
+<?php
+
+//require_once  $_SERVER['DOCUMENT_ROOT']."/bootstrap.php";
+require_once  $_SERVER['DOCUMENT_ROOT']."/controller/init.php";
+//require_once 'export.php';
+
+//$redirect = "view\index.php";
+//$redirect = "humanus\humanus.html";
+
 phpinfo();
+//$redirect = "web/index.php";
+//redirecionar($redirect, '_self');
+$x = 10;
+$y = 5;
+
 ?>
